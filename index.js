fa56de44f7e53a0ac0976f474a410a4cb2b3f8c8
Davi Henrique <72770796+davigtr@users.noreply.github.com>
2021-07-05 13:02:46 -0300

    arquivos

--- index.js
+++ index.js
@@ -6,12 +6,40 @@ function criaCalculadora() {
 
     display: document.getElementById('visor'),
 
+    btnClear: document.querySelector('.btn-clear'),
+
 
     /* métodos */
 
     get inicia(){
       console.log('to aqui')
       this.cliqueBtn()
+    },
+
+    clearDisplay(){
+      this.display.value = ' '
+    },
+
+    deleteUm(){
+      this.display.value = this.display.value.slice(0, -1)
+    },
+
+    realizaConta(){
+      let conta = this.display.value
+
+      try {
+        conta = eval(conta)
+
+        if(conta === " " || Number.isNaN(conta) ||typeof(conta) !== 'number'){
+          alert('Erro1')
+          return
+        }
+        
+        this.display.value = String(conta)
+
+      } catch (e) {
+        alert('Erro')
+      }
     },
 
     cliqueBtn(){
@@ -22,6 +50,19 @@ function criaCalculadora() {
         if( el.classList.contains('btn-num')){
           this.btnParaDisplay(el.innerText)
         }
+
+        if(el.classList.contains('btn-clear')){
+          this.clearDisplay()
+        }
+
+        if(el.classList.contains('btn-del')){
+          this.deleteUm()
+        }
+
+        if(el.classList.contains('btn-igual')){
+          this.realizaConta()
+        }
+
       }.bind(this) )
     },
 
